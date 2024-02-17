4b8b07f569a130f891965da3a147f5f3570ed156
saqib shah <xaqibshah@gmail.com>
2024-02-17 12:14:46 +0500

    update write function

--- systemCall/01_write.c
+++ systemCall/01_write.c
@@ -1,9 +1,10 @@
 #include<unistd.h>
-
+#include<stdio.h>
 int main(){
 
 // 	    fd	constat             sizeof	
-	write(1, "Hello Write System Call", 22);
+	int n=	write(1, "Hello Write System Call", 22);
+	printf("\nvalue of n is %d \n", n);
 
 //fd 1 input
 //fd 2 output
