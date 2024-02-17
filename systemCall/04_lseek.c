acf0c78b2b34d72b3b60c7db5827eaf165d8b3a7
saqib shah <xaqibshah@gmail.com>
2024-02-17 15:10:21 +0500

    Finish lseek working

--- systemCall/04_lseek.c
+++ systemCall/04_lseek.c
@@ -7,10 +7,15 @@
 int main(){
 
 	int n, f, f1;
-	char buff[10]
+	char buff[10];
 	f=open("seeking", O_RDWR);  // 1234567890abcdefghijx1x2x3x4x5
-	read(f,buff,10);
+	read(f,buff,10); //1234567890
 	write(1,buff,10);
-	read(f,buff,10);
+
+			//wehre SEEK_CUR,start Selecting from current position	lseek(f, 10, SEEK_CUR);   
+	 // SEEK_SET, start selecting from Begning
+	f1=lseek(f,-11, SEEK_END);
+	printf("\n Pointer is at %d position \n", f1);	
+	read(f,buff,10);//abcdefghij
 	write(1,buff,10);
 }
