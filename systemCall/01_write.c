34a08d99ce0227e86181c00753f36877a07c5605
saqib shah <xaqibshah@gmail.com>
2024-02-17 12:09:07 +0500

    start working with system call with write

---
+++ systemCall/01_write.c
@@ -0,0 +1,12 @@
+#include<unistd.h>
+
+int main(){
+
+// 	    fd	constat             sizeof	
+	write(1, "Hello Write System Call", 22);
+
+//fd 1 input
+//fd 2 output
+//fd 3 error
+
+}
---
+++ systemCall/basicCall.c
@@ -0,0 +1,23 @@
+#include<unistd.h>
+#include<stdio.h>
+#include<fcntl.h>
+
+
+int main(){
+
+int fd;
+char buffer[80];
+char msg [50] = "Hello System Call";
+fd = open ("check.txt", O_RDWR);
+printf("fd= %d", fd);
+if (fd != -1);
+{
+printf("\n check.txt opened with read write access \n");
+write(fd, msg , sizeof(msg));
+lseek(fd, 0, SEEK_SET);
+read(fd, buffer, sizeof(msg));
+printf("\n %s was written to my file \n" , buffer);
+close(fd);
+}
+return 0;
+}
