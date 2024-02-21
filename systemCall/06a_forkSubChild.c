fc56620078a14d6cf786f91c985efb8bdc9fc533
saqib shah <xaqibshah@gmail.com>
2024-02-21 22:44:58 +0500

    update fork and sub child process

--- systemCall/06a_forkSubChild.c
+++ systemCall/06a_forkSubChild.c
@@ -13,16 +13,25 @@ int main(){
 	q = fork();
 
  if (q==0){ //child process
-
-
+	printf("The value of q is: %d \n", q); 
 	printf("I am child having pid %d \n", getpid());
 	printf("My parent's pid is %d \n", getppid());
-}
+	pid_t subProc;
+	subProc = fork();
 
+	if (q==0){
+		printf("i'm sub child process %d \n", getpid());
+		printf("my  parent process is %d \n", getppid());
+}
+	else{
+		printf("i'm sub parent PID is %d \n",getpid());
+		printf("i'm sub child having PID is %d \n", q); 
+}
+}
 else  // q>0 parent process
 {
-	printf("My Child's pid is %d \n", q);
 	printf("I am Parent having pid %d \n", getpid());
+	printf("My Child's pid is %d \n", q);
 
 
 }
